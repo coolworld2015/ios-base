2ee63143f3a889eea90dc3f2c30c5b2aa3c15790
coolworld2015 <wintermute2015@ukr.net>
2017-04-26 11:05:22 +0300

    [login] - refactoring

--- app/src/android/app/login.js
+++ app/src/android/app/login.js
@@ -123,8 +123,7 @@ class Login extends Component {
                             borderColor: 'lightgray',
                             borderRadius: 5,
                             color: 'black',
-                            backgroundColor: 'white',
-                            fontWeight: 'bold'
+                            backgroundColor: 'white'
                         }}
                         value={this.state.username}
                         placeholder='Login'>
@@ -146,8 +145,7 @@ class Login extends Component {
                             borderColor: 'lightgray',
                             borderRadius: 5,
                             color: 'black',
-                            backgroundColor: 'white',
-                            fontWeight: 'bold'
+                            backgroundColor: 'white'
                         }}
                         value={this.state.password}
                         placeholder='Password'
